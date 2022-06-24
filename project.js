7c3b9bbb9573482a59dc21db22d999c25958d28e
Rutvik-R <rutvikranpariya1221@gmail.com>
2022-06-24 16:28:04 +0530

    changing menu or js file

---
+++ project.js
@@ -0,0 +1,18 @@
+let Sada_Dosa = {
+    image : "image" ,
+    price : 60 
+
+}
+
+
+
+var Snacks = []
+var Dosa_Uttapam = [Sada_Dosa]
+var Pakoda = []
+var Pav_Bhaji = []
+var Sandwich = []
+var Pizza = []
+var Cold_Hot = []
+var Speciality = []
+let str = "";
+
